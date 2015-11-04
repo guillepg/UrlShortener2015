670bd44a202907930a63b64f826dfb9bf954861b
guillepg <guillepg@outlook.es>
2015-11-04 11:22:43 +0100

    guille's first commit

--- dim-gray/src/main/java/urlshortener2015/dim-gray/web/UrlShortenerControllerWithLogs.java
+++ dim-gray/src/main/java/urlshortener2015/dim-gray/web/UrlShortenerControllerWithLogs.java
@@ -2,6 +2,8 @@ package urlshortener2015.demo.web;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.Logger;
+import org.hibernate.validator.internal.util.logging.LoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
