ee02332d88ac1076d0d6862b853c4f2d3a1ddf5a
Ramon Jose Arellano Bitrian <ramonmoxo@gmail.com>
2015-11-17 04:18:36 +0100

    Everithing is working fine

--- dimGray/src/main/java/urlshortener2015/dimGray/config/PersistenceContext.java
+++ dimGray/src/main/java/urlshortener2015/dimGray/config/PersistenceContext.java
@@ -4,10 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-import urlshortener2015.dimGray.repository.ClickRepository;
-import urlshortener2015.dimGray.repository.ClickRepositoryImpl;
-import urlshortener2015.dimGray.repository.ShortURLRepository;
-import urlshortener2015.dimGray.repository.ShortURLRepositoryImpl;
+
+import urlshortener2015.common.repository.ClickRepository;
+import urlshortener2015.common.repository.ClickRepositoryImpl;
+import urlshortener2015.common.repository.ShortURLRepository;
+import urlshortener2015.common.repository.ShortURLRepositoryImpl;
 
 @Configuration
 public class PersistenceContext {
